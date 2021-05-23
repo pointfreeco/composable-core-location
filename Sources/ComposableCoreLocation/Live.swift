195204c25b921d7607aeb8777bfbc0a9fe740734
Mihael Cholakov <myurieff@gmail.com>
2021-05-23 15:12:07 +0300

    Proper identation

--- Sources/ComposableCoreLocation/Live.swift
+++ Sources/ComposableCoreLocation/Live.swift
@@ -161,17 +161,17 @@ extension LocationManager {
       }
     #endif
 		
-		#if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
-		manager.startMonitoringSignificantLocationChanges = { id in 
-			.fireAndForget { dependencies[id]?.manager.startMonitoringSignificantLocationChanges() }
-		}
-		#endif
-		
-		#if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
-		manager.stopMonitoringSignificantLocationChanges = { id in
-			.fireAndForget { dependencies[id]?.manager.stopMonitoringSignificantLocationChanges() }
-		}
-		#endif
+    #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
+    manager.startMonitoringSignificantLocationChanges = { id in 
+      .fireAndForget { dependencies[id]?.manager.startMonitoringSignificantLocationChanges() }
+    }
+    #endif
+    
+    #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
+    manager.stopMonitoringSignificantLocationChanges = { id in
+      .fireAndForget { dependencies[id]?.manager.stopMonitoringSignificantLocationChanges() }
+    }
+    #endif
 
     return manager
   }()
