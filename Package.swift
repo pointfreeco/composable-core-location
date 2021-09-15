ce5a0d0258b885a0cb366ffd95a4ed29e61fca7f
Andrey Zhukov <git@andrey.se>
2021-09-15 19:57:11 +0200

    Pin TCA to 0.27.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .branch("main"))
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "0.27.1"))
   ],
   targets: [
     .target(
