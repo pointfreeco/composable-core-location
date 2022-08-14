f4ec5d186b9e6c6dc79e26f0d1dc92870cab16b5
Roberto Casula <roberto@casula.dev>
2022-08-14 09:23:02 +0200

    Convert LocationManagerDelegate to use continuation instead of Effect

--- Sources/ComposableCoreLocation/Live.swift
+++ Sources/ComposableCoreLocation/Live.swift
@@ -247,24 +247,24 @@ extension LocationManager {
 }
 
 private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
-  let subscriber: EffectPublisher<LocationManager.Action, Never>.Subscriber
+  let continuation: AsyncStream<LocationManager.Action>.Continuation
 
-  init(_ subscriber: EffectPublisher<LocationManager.Action, Never>.Subscriber) {
-    self.subscriber = subscriber
+  init(continuation: AsyncStream<LocationManager.Action>.Continuation) {
+    self.continuation = continuation
   }
 
   func locationManager(
     _ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus
   ) {
-    self.subscriber.send(.didChangeAuthorization(status))
+    self.continuation.yield(.didChangeAuthorization(status))
   }
 
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    self.subscriber.send(.didFailWithError(LocationManager.Error(error)))
+    self.continuation.yield(.didFailWithError(LocationManager.Error(error)))
   }
 
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    self.subscriber.send(.didUpdateLocations(locations.map(Location.init(rawValue:))))
+    self.continuation.yield(.didUpdateLocations(locations.map(Location.init(rawValue:))))
   }
 
   #if os(macOS)
@@ -285,7 +285,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
     func locationManager(
       _ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?
     ) {
-      self.subscriber.send(
+      self.continuation.yield(
         .didFinishDeferredUpdatesWithError(error.map(LocationManager.Error.init))
       )
     }
@@ -293,31 +293,31 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
 
   #if os(iOS) || targetEnvironment(macCatalyst)
     func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager) {
-      self.subscriber.send(.didPauseLocationUpdates)
+      self.continuation.yield(.didPauseLocationUpdates)
     }
   #endif
 
   #if os(iOS) || targetEnvironment(macCatalyst)
     func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
-      self.subscriber.send(.didResumeLocationUpdates)
+      self.continuation.yield(.didResumeLocationUpdates)
     }
   #endif
 
   #if os(iOS) || os(watchOS) || targetEnvironment(macCatalyst)
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-      self.subscriber.send(.didUpdateHeading(newHeading: Heading(rawValue: newHeading)))
+      self.continuation.yield(.didUpdateHeading(newHeading: Heading(rawValue: newHeading)))
     }
   #endif
 
   #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-      self.subscriber.send(.didEnterRegion(Region(rawValue: region)))
+      self.continuation.yield(.didEnterRegion(Region(rawValue: region)))
     }
   #endif
 
   #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-      self.subscriber.send(.didExitRegion(Region(rawValue: region)))
+      self.continuation.yield(.didExitRegion(Region(rawValue: region)))
     }
   #endif
 
@@ -325,7 +325,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
     func locationManager(
       _ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion
     ) {
-      self.subscriber.send(.didDetermineState(state, region: Region(rawValue: region)))
+      self.continuation.yield(.didDetermineState(state, region: Region(rawValue: region)))
     }
   #endif
 
@@ -333,7 +333,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
     func locationManager(
       _ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error
     ) {
-      self.subscriber.send(
+      self.continuation.yield(
         .monitoringDidFail(
           region: region.map(Region.init(rawValue:)), error: LocationManager.Error(error)))
     }
@@ -341,7 +341,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
 
   #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
     func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
-      self.subscriber.send(.didStartMonitoring(region: Region(rawValue: region)))
+      self.continuation.yield(.didStartMonitoring(region: Region(rawValue: region)))
     }
   #endif
 
@@ -350,7 +350,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
       _ manager: CLLocationManager, didRange beacons: [CLBeacon],
       satisfying beaconConstraint: CLBeaconIdentityConstraint
     ) {
-      self.subscriber.send(
+      self.continuation.yield(
         .didRangeBeacons(
           beacons.map(Beacon.init(rawValue:)), satisfyingConstraint: beaconConstraint
         )
@@ -363,7 +363,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
       _ manager: CLLocationManager, didFailRangingFor beaconConstraint: CLBeaconIdentityConstraint,
       error: Error
     ) {
-      self.subscriber.send(
+      self.continuation.yield(
         .didFailRanging(beaconConstraint: beaconConstraint, error: LocationManager.Error(error))
       )
     }
@@ -371,7 +371,7 @@ private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
 
   #if os(iOS) || targetEnvironment(macCatalyst)
     func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
-      self.subscriber.send(.didVisit(Visit(visit: visit)))
+      self.continuation.yield(.didVisit(Visit(visit: visit)))
     }
   #endif
 }
