d2fd9d248fbde3ea6fde69b210affa6178f3fd21
Roberto Casula <16337140+rcasula@users.noreply.github.com>
2023-08-03 07:54:06 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.2")
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0")
   ],
   targets: [
     .target(
