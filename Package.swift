00d426d974a218327b1fbce837ebb5cced94071f
André Pacheco Neves <andre.neves@mindera.com>
2023-09-06 12:16:09 +0100

    Comment compiler concurrency warnings from Package.swift

--- Package.swift
+++ Package.swift
@@ -35,12 +35,12 @@ let package = Package(
   ]
 )
 
-for target in package.targets {
-  target.swiftSettings = target.swiftSettings ?? []
-  target.swiftSettings?.append(
-    .unsafeFlags([
-      "-Xfrontend", "-warn-concurrency",
-      "-Xfrontend", "-enable-actor-data-race-checks",
-    ])
-  )
-}
+//for target in package.targets {
+//  target.swiftSettings = target.swiftSettings ?? []
+//  target.swiftSettings?.append(
+//    .unsafeFlags([
+//      "-Xfrontend", "-warn-concurrency",
+//      "-Xfrontend", "-enable-actor-data-race-checks",
+//    ])
+//  )
+//}
